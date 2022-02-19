8a411b1a8254d6525b17ab1f1b2f0f9fd0866abf
Paul OE <paulehiks@gmail.com>
2022-02-19 01:21:53 +0100

    created ICO contract

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xBf0fe061EFcFc42A4e02e2b6E2D9044fB4497Bb0";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
